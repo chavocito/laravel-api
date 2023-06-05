3beaa77fb4d82f6e0e4509821932e0b243cbc32e
Chavocito <fiifiowusu@aol.com>
2023-06-05 21:54:08 +0000

    add api routes and controllers

--- lara-api/app/Http/Controllers/Api/v1/InvoiceController.php
+++ lara-api/app/Http/Controllers/Api/v1/InvoiceController.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api\v1;
 
-use App\Models\Customer;
-use App\Http\Requests\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
+use App\Models\Invoice;
+use App\Http\Requests\StoreInvoiceRequest;
+use App\Http\Requests\UpdateInvoiceRequest;
+use App\Http\Controllers\Controller;
 
-class CustomerController extends Controller
+class InvoiceController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -14,6 +15,7 @@ class CustomerController extends Controller
     public function index()
     {
         //
+        return Invoice::all();
     }
 
     /**
@@ -27,7 +29,7 @@ class CustomerController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreCustomerRequest $request)
+    public function store(StoreInvoiceRequest $request)
     {
         //
     }
@@ -35,7 +37,7 @@ class CustomerController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Customer $customer)
+    public function show(Invoice $invoice)
     {
         //
     }
@@ -43,7 +45,7 @@ class CustomerController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Customer $customer)
+    public function edit(Invoice $invoice)
     {
         //
     }
@@ -51,7 +53,7 @@ class CustomerController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateCustomerRequest $request, Customer $customer)
+    public function update(UpdateInvoiceRequest $request, Invoice $invoice)
     {
         //
     }
@@ -59,7 +61,7 @@ class CustomerController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Customer $customer)
+    public function destroy(Invoice $invoice)
     {
         //
     }
